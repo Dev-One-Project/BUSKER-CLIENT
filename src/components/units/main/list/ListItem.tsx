5c3ed0b222cafc902a17ddc5b11d64868b0583f8
aggie97 <4ggie1997@gmail.com>
2023-03-09 15:37:00 +0900

    :alien: (list): MODIFY: 백엔드 API 변경으로 인한 코드 수정
    fetchBoardsBySearch 인자값 변경 및 리스트 페이지 한 줄에 공연 개수 4개로 변경

--- src/components/units/main/list/ListItem.tsx
+++ src/components/units/main/list/ListItem.tsx
@@ -51,7 +51,7 @@ const ListItem = ({ board, onClickListItem }: IListItemProps) => {
             </span>
           </ItemInfo>
           <ItemInfo>
-            장르 <span>{board?.category.name}</span>
+            장르 <span>{board?.category?.name}</span>
           </ItemInfo>
         </ContentBox>
       </Wrapper>
